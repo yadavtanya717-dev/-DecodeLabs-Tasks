5f091db8352663f7a25f8ad579000d5a9db7e4a4
yadavtanya717-dev <yadavtanya717@gmail.com>
2026-07-07 18:45:37 +0530

    Connect frontend to deployed backend

--- Project-2-AI-Copywriting-And-Tone-Transformer/frontend/src/services/api.js
+++ Project-2-AI-Copywriting-And-Tone-Transformer/frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = "https://project-2-backend-rbtk.onrender.com";
 
 export const generateCopy = async (data) => {
   const res = await axios.post(`${BASE_URL}/generate`, data);
